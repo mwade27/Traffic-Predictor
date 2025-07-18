1bbc134115904485e41976b345780b15e029283c
mwade27 <mew2@uab.edu>
2025-07-17 21:19:36 -0500

    should be ready for final run tommorrow combined severity 1 and 2 and 3 with 4 since it was too similar

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import pandas as pd
 from eda import severity_distribution, weather_condition_distribution, top_accident_locations, accident_by_timeofday
-from mlmodel import check_data_integrity, train_test_split_data
+from mlmodel import check_data_integrity,binning_columns, train_test_split_data
 #May need to drop Start_time and End_time later after feature engineering
 filepath = 'data/US_Accidents_March23.csv'
 def load_data(filepath):
     df = pd.read_csv(filepath)
     print("Data loaded successfully.")
     
-    columns_to_drop = ['Source','Description','Civil_Twilight','Nautical_Twilight','Country',
-                       'Weather_Timestamp','Wind_Direction','Zipcode', 'Airport_Code','Astronomical_Twilight','ID']
+    columns_to_drop = ['Source','Description','Civil_Twilight','Nautical_Twilight','Country'
+                       ,'Amenity','Bump','Crossing','Give_Way','Junction','County','Street'
+                       ,'No_Exit','Railway','Roundabout','Station','Stop','Traffic_Calming'
+                       ,'Traffic_Signal','Turning_Loop','Sunrise_Sunset','Zipcode', 'Airport_Code','Astronomical_Twilight','ID']
+    
     # Drop unnecessary columns
     df.drop(columns = columns_to_drop, axis=1, inplace=True)
-    #print(df.head())
+    
+    
     return df
 
 """ 
@@ -21,12 +25,12 @@ def handle_missing_values(df):
     # Check for missing values
     print("\n Top 20 columns with missing Values ")
     print(df.isnull().sum().sort_values(ascending=False).head(20))
-    
+    exclude_cols = ['Precipitation(in)','Wind_Chill(F)']  # columns to skip
     # Drop rows with missing values in 'Severity' column since its are target variable
     df.dropna(subset=['Severity'], inplace=True)
     # Calculate the percentage of missing values in each column
     missing = df.isnull().mean()
-    high_missing_cols = missing[missing > 0.4].index
+    high_missing_cols = [col for col in missing[missing > 0.4].index if col not in exclude_cols]
     df.drop(columns=high_missing_cols, inplace=True)
     
     # Fill missing values for categorical and numerical columns
@@ -36,7 +40,10 @@ def handle_missing_values(df):
         
     num_columns = df.select_dtypes(include=['float64', 'int64']).columns
     for col in num_columns:
-        df[col] = df[col].fillna(df[col].median())
+        if col not in exclude_cols:
+            df[col] = df[col].fillna(df[col].median())
+        elif col in exclude_cols:
+            df[col] = df[col].fillna(0)
         
 #Splitting time columns into separate features for better analysis and visualization
 def time_splitting(df):
@@ -80,6 +87,10 @@ def time_features(df):
      #drop the original 'Start_Time' and 'End_Time' columns
     df.drop(columns=['Start_Time', 'End_Time'], axis=1 ,inplace=True)
     
+def drop_years(df, years_to_drop=[2016, 2017, 2018]):
+    df.drop(df[df['Year'].isin(years_to_drop)].index, inplace=True)
+    print(f"Dropped rows for years: {years_to_drop}")
+    
         
 def feature_engineering(df):
     convert_booleans(df)
@@ -95,18 +106,25 @@ def feature_engineering(df):
 def main():
     # Load the dataset
     df = load_data('data/US_Accidents_March23.csv')
+    
     handle_missing_values(df)
     time_splitting(df)
+    drop_years(df, [2016, 2017, 2018])
     feature_engineering(df)
     
-    #print(df.columns)
-    
+    print(df.columns)
+    print(len(df))
+    df.to_csv('data/US_Accidents_March23_cleaned.csv', index=False)
     #print(df_ml.columns)
     
-    #df_ml = df.copy()
-    #df_ml = check_data_integrity(df_ml)
-    #print(df['Severity'].value_counts())
-    #train_test_split_data(df_ml)
+    df_ml = df.copy()
+    df_ml = binning_columns(df_ml)
+    df_ml = check_data_integrity(df_ml)
+    
+    print("\nData integrity check completed.")
+    print(df_ml['Severity'].value_counts())
+    #print(df_ml.columns)
+    train_test_split_data(df_ml)
     
     
    
--- mlmodel.py
+++ mlmodel.py
@@ -11,9 +11,54 @@ from imblearn.under_sampling import RandomUnderSampler
 from collections import Counter
 import joblib
 import datetime
+import os
+
+def binning_columns(df):
+    # Need to bin the 'Temperature(F)', 'Humidity(%)', and 'Visibility(mi)', Pessure, Precipitation and windSpeedcolumns
+    temp_bins = [-np.inf, 10, 20, 30, 40, 50, 60, 70, 80, 90, np.inf]
+    temp_labels = [
+    'Extreme Cold', 'Frigid', 'Freezing', 'Cold', 'Chilly',
+    'Mild', 'Warm', 'Hot', 'Very Hot', 'Extreme Heat']
+    df['Temperature_Binned'] = pd.cut(df['Temperature(F)'], bins=temp_bins, labels=temp_labels)
+    
+    humid_bins = [-np.inf,30,50,85,100, np.inf]
+    humid_labels = ['Dry', 'Comfortable', 'Humid', 'Very Humid', 'Oppressive']
+    df['Humidity_Binned'] = pd.cut(df['Humidity(%)'], bins=humid_bins, labels=humid_labels)
+    
+    press_bins = [-np.inf, 29.5, 29.9, 30.2, 30.5, np.inf]
+    press_labels =['Low', 'Below Normal', 'Normal', 'High', 'Very High']
+    df['Pressure_Binned'] = pd.cut(df['Pressure(in)'], bins=press_bins, labels=press_labels)
+    
+    vis_bins = [-np.inf, 0.5, 1, 2, 4, 6, 10, 15, np.inf]
+    vis_labels = [
+    'Zero', 'Very Poor', 'Poor', 'Low', 'Moderate',
+    'High', 'Very High', 'Excellent']
+    df['Visibility_Binned'] = pd.cut(df['Visibility(mi)'], bins=vis_bins, labels=vis_labels)
+    
+    windspeed_bins = [-np.inf, 5, 10, 15, 20, 30, np.inf]
+    windspeed_labels = ['Very Low', 'Low', 'Moderate', 'High', 'Very High', 'Extreme']
+    df['Wind_Speed_Binned'] = pd.cut(df['Wind_Speed(mph)'], bins=windspeed_bins, labels=windspeed_labels)
+    
+    precip_bins = [-np.inf, 0, 0.01, 0.1, 0.25, 0.5, 1, 2, 5, np.inf]
+    precip_labels = ['None', 'Trace', 'Light', 'Moderate', 'Heavy','Very Heavy', 'Extreme', 'Severe', 'Catastrophic']
+    df['Precipitation_Binned'] = pd.cut(df['Precipitation(in)'], bins=precip_bins, labels=precip_labels)
+    
+    windchill_bins = [-np.inf, 0, 10, 20, 32, 40, 50, 60, 70, np.inf]
+    windchill_labels = ['Extreme Cold', 'Frigid', 'Freezing', 'Cold', 'Chilly','Mild', 'Warm', 'Hot', 'Very Hot']
+    df['Wind_Chill_Binned'] = pd.cut(df['Wind_Chill(F)'], bins=windchill_bins, labels=windchill_labels)
+
+    hour_bins = [0,7,10, 16, 19, 24]
+    hour_labels = ['Early_Morning','Morning_Rush', 'Midday', 'Evening_Rush', 'Night']
+    df['Start_Hour_Binned'] = pd.cut(df['Start_Hour'], bins=hour_bins, labels=hour_labels)
+    #add binning for Precipitation and Wind_Chill
+    df.drop(columns=['Temperature(F)', 'Humidity(%)', 'Pressure(in)', 'Visibility(mi)', 'Wind_Speed(mph)','Precipitation(in)','Wind_Chill(F)','Start_Hour'], inplace=True, errors='ignore')
+    
+    print("\n Binning Completed:")
+    return df
 
 def check_data_integrity(df):
     print("\n Checking for missing values after processing:")
+    df.dropna(subset=['Start_Hour_Binned'], inplace=True)
     print(df.isnull().sum().sort_values(ascending=False).head(20))
     for col in df.columns:
         if df[col].isnull().any():
@@ -30,7 +75,8 @@ def check_data_integrity(df):
 
     
     print("\nChecking for number of unique values in each column:")
-
+    #exclude_columns = ['Severity','Temperature_Binned', 'Humidity_Binned', 'Pressure_Binned', 'Visibility_Binned', 'Wind_Speed_Binned']
+   
     for col in df.columns:
         unique_count = df[col].nunique()
         # Drop columns with more than 500 unique values
@@ -46,74 +92,81 @@ def check_data_integrity(df):
 
     # encoding categorical variables
     
-    categorical_columns = df.select_dtypes(include=['object']).columns
+    categorical_columns = df.select_dtypes(include=['object','category']).columns
     print(f"\nCategorical columns before encoding: {categorical_columns}")
     if len(categorical_columns) > 0:
         print(f"One-hot encoding these columns: {categorical_columns}")
         df = pd.get_dummies(df, columns=categorical_columns, drop_first=True)
     #print(df.columns.to_list())
     #print("\nOne-hot encoding applied to categorical columns.")
-    #Replace the level 4 severity with level 3 since 4 is underrepresented in the dataset
-    df['Severity'] = df['Severity'].replace({4:3})
-    df['Severity'] = df['Severity'].replace({1:2})  
-    # Replace level 1 with level 2 for better balance
     
-    df.drop(columns=['Year','End_Hour','Humidity(%)'],inplace = True)  # Ensure no NaN in target variable
-    print("\nFinal DataFrame info:")
-    print(df.info())
+    df['Severity'] = df['Severity'].replace({4:3,1:2})
+    
+    
+    df.drop(columns=['Year','End_Hour','Start_Lat','Start_Lng','End_Date','End_Hour','Start_Date'],axis=1,inplace = True,errors='ignore')  # Ensure no NaN in target variable
+    #print("\nFinal DataFrame info:")
+    #print(df.columns.to_list())
     
     return df
 
     
 
 
-
+#Thursday might be good to test with different estimators, 100,125,150 or test out XGBoost
 def train_test_split_data(df):
+
 
     #Split the data into features and target variable
     # X is all columns except 'Severity'
     X = df.drop(columns=['Severity'])
     # y is the 'Severity' column
     y = df['Severity']
-    
+   
+
 
     """
     Training with SMOTE (Synthetic Minority Over-sampling Technique) to handle class imbalance
     Should help with the imbalance in the dataset with the level 4 severity being underrepresented
     Dont use SMOTE in the testing phase, only in the training phase
     """
-    
-
-
+   
     print("Testing is starting")
     start_time = time.time()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    #rus = RandomUnderSampler(sampling_strategy={2: 400000, 3: 300000, 1: 50000}, random_state=42)
-    #X_train_under,y_train_under = rus.fit_resample(X_train, y_train)
     
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+   
     sm = SMOTE(random_state=42)
     X_train_balanced, y_train_balanced = sm.fit_resample(X_train, y_train)
  
     # random_state = ensures reproducibility
-    # instead of balanced, gonna try class_weight={1: 8, 2:1, 3:4}
-    model = RandomForestClassifier(n_estimators=100, random_state=42)  # Using balanced class weights
-
+    model = RandomForestClassifier(n_estimators=100, random_state=42,class_weight = 'balanced')  # Using balanced class weights
     
-    #model.fit(X_train_under, y_train_under)
+   
+   
     # Fit the model on the training data
     model.fit(X_train_balanced, y_train_balanced)
-
+   
+    # 📏 Tree Depth Analysis
+    depths = [estimator.tree_.max_depth for estimator in model.estimators_]
+    print(f"\nTree Depths → Avg: {np.mean(depths):.2f}, Max: {np.max(depths)}")
+   
     print("Before resampling:", y_train.value_counts())
     #print("After undersampling:", pd.Series(y_train_under).value_counts())
     print("After SMOTE:", pd.Series(y_train_balanced).value_counts())
-
-
+    y_train_pred = model.predict(X_train_balanced)
+    train_accuracy = accuracy_score(y_train_balanced, y_train_pred)
+    train_error = 1 - train_accuracy
+   
     # Make predictions on the test data
+    
     y_pred = model.predict(X_test)
-    end_time = time.time()
+    test_accuracy = accuracy_score(y_test, y_pred)
+    test_error = 1 - train_accuracy
+   
 
-    print(f"Model training and prediction completed in {end_time - start_time:.2f} seconds.")
-    print("Accuracy of the model:", accuracy_score(y_test, y_pred))
+
+   
+    print("Accuracy of the model:", test_accuracy)
     print("\nClassification Report:", classification_report(y_test, y_pred))
     print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
     feature_importances = pd.Series(model.feature_importances_, index=X.columns)
@@ -121,9 +174,18 @@ def train_test_split_data(df):
     plt.title('Top 15 Feature Importances')
     plt.show()
     
+    end_time = time.time()
+    print(f"Model training and prediction completed in {end_time - start_time:.2f} seconds.")
+    
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M")
     joblib.dump(model, f'accident_severity_predictor_{timestamp}.pkl')
 
     
+    
+    
+    
+    
+
+
 
 
